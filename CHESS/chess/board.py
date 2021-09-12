9ad40e39e613f286a897567d5a444ca8f8d418a2
jjava1234 <81831375+jjava1234@users.noreply.github.com>
2021-09-12 00:13:04 -0400

    Update board.py

--- CHESS/chess/board.py
+++ CHESS/chess/board.py
@@ -11,6 +11,7 @@ class Board():
         self.board_layout = []
         self.game = game
         self.win = window
+        self.selected = None
         self.create_board()
         
 
@@ -25,17 +26,17 @@ class Board():
         board = self.board_layout
         for row in range(8):
             board.append([])
-            for col in range(8):    
+            for col in range(8):
                 if row < 2:
                     if row == 0:
                         board[row].append(bPieces[col](col, row, (0,0,0)))
                     else:
-                        board[row].append(pawn.Pawn(col, row, (0,0,0)))
+                        board[row].append(pawn.Pawn(col, row, (0,0,0), 1))
                 elif row > 5:
                     if row == 7:
                         board[row].append(wPieces[col](col, row, (255,255,255)))
                     else: 
-                        board[row].append(pawn.Pawn(col, row, (255,255,255)))
+                        board[row].append(pawn.Pawn(col, row, (255,255,255), -1))
                 else:
                     board[row].append(0)
         self.draw_pieces()
@@ -54,3 +55,33 @@ class Board():
                         for wPiece in wP_images:
                             if wPiece == piece.pName:
                                 self.win.blit(wP_images[wPiece], (((piece.x*80 + 40) - bP_images[bPiece].get_width()//2, (piece.y*80 + 40) - bP_images[bPiece].get_height()//2)))
+    
+    
+    def draw_moves(self, valid_moves):
+        print(valid_moves)
+        for move in valid_moves:
+            pygame.draw.circle(self.win, (0,0,255), (move[0]*80+40, move[1]*80+40), 15)
+
+    def undraw_moves(self, valid_moves):
+        for move in valid_moves:
+            if move[0]%2 == move[1]%2:
+                print(move[0], move[1])
+                pygame.draw.rect(self.win, (210,180,140), (move[0]*80, move[1]*80, 80, 80))
+            else:
+                pygame.draw.rect(self.win, (111,78,55), (move[0]*80, move[1]*80, 80, 80))
+
+
+    def select(self, x, y):
+        if piece := self.board_layout[y//80][x//80]:
+            if self.selected != piece:
+                if self.selected:
+                    self.undraw_moves(self.selected.valid_moves)            
+                    self.selected.valid_moves.clear()
+                self.selected = piece
+                self.draw_moves(self.selected.calc_moves(self.board_layout))
+
+                    
+        #else:
+
+
+#move function: see if opponent piece's new pos puts enemy's king in danger 
