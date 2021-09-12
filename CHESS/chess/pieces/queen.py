0f70f8eaef4ca386842793e1152b39abbae07840
jjava1234 <81831375+jjava1234@users.noreply.github.com>
2021-09-12 00:14:12 -0400

    Delete queen.py

--- CHESS/chess/pieces/queen.py
+++
@@ -1,9 +0,0 @@
-class Queen():
-    def __init__(self, x, y, color):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.pName = "queen"
-
-    def move():
-        pass
