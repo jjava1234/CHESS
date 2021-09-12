4ee85687a10b18256433ea1ec58caa2c1b21d761
jjava1234 <81831375+jjava1234@users.noreply.github.com>
2021-09-12 00:13:57 -0400

    Delete knight.py

--- CHESS/chess/pieces/knight.py
+++
@@ -1,9 +0,0 @@
-class Knight():
-    def __init__(self, x, y, color):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.pName = "knight"
-
-    def move():
-        pass
