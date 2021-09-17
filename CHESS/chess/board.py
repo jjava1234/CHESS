05973763da1dc2a9f9ef3a39c67288f08f2e0ba1
jjava1234 <81831375+jjava1234@users.noreply.github.com>
2021-09-17 00:04:17 -0400

    Update board.py

--- CHESS/chess/board.py
+++ CHESS/chess/board.py
@@ -1,4 +1,4 @@
-from pieces.pieces import *
+from pieces import *
 from constants import *
 
 bPieces = [Rook, Horse, Bishop, Queen, King, Bishop, Horse, Rook]
@@ -11,10 +11,8 @@ class Board():
         self.game = game
         self.win = window
         self.selected = None
-        self.valid_moves = None
         self.create_board()
         
-
 
     def create_board(self):
         self.win.fill((111,78,55))
@@ -30,51 +28,51 @@ class Board():
             for col in range(8):
                 if row < 2:
                     if row == 0:
-                        board[row].append(bPieces[col](col, row, (0,0,0), "w" + bPieces[col].__name__[0], self.win))
+                        board[row].append(bPieces[col](col, row, (0,0,0), f"b{bPieces[col].__name__[0]}", self.win))
                     else:
                         board[row].append(0)
-                        
-                        #board[row].append(Pawn(col, row, (0,0,0), 1, "wP", self.win))
+
+            #            board[row].append(Pawn(col, row, (0,0,0), 1, "bP", self.win))
                 elif row > 5:
                     if row == 7:
-                        board[row].append(wPieces[col](col, row, (255,255,255), "b" + wPieces[col].__name__[0], self.win))
+                        board[row].append(wPieces[col](col, row, (255,255,255), f"w{wPieces[col].__name__[0]}", self.win))
                     else: 
-                        board[row].append(0)
+                          board[row].append(0)
 
-                        #board[row].append(Pawn(col, row, (255,255,255), -1, "bP", self.win))
+         #               board[row].append(Pawn(col, row, (255,255,255), -1, "wP", self.win))
                 else:
                     board[row].append(0)
-    
+        self.draw_pieces()
 
-                
+
+    def draw_pieces(self):
+        pass
     
     def draw_moves(self, valid_moves):
-        if any(valid_moves):
-            for move in valid_moves:
-                pygame.draw.circle(self.win, (0,0,255), (move[0]*80+40, move[1]*80+40), 15)
-            return valid_moves
+        for move in valid_moves:
+            pygame.draw.circle(self.win, (0,0,255), (move[0]*80+40, move[1]*80+40), 15)
+        return valid_moves
 
-
-    def undraw_moves(self, valid_moves):
-
+    def undraw_moves(self, board, valid_moves):
         colors = [(210,180,140), (111,78,55)]
-        #colors = [(0,0,0), (255,255,255)]
-        if any(valid_moves):
-            for move in valid_moves:
-                pygame.draw.rect(self.win, (colors[(move[0]+move[1])%2]), (move[0]*80, move[1]*80, 80, 80))
-
+        for move in valid_moves:
+            pygame.draw.rect(self.win, colors[(move[0]+move[1])%2], (move[0]*80, move[1]*80, 80, 80))     
+            if piece := board[move[1]][move[0]]:
+                piece.draw_piece(self.win)
 
     def select(self, x, y):
         if piece := self.board_layout[y//80][x//80]:
             if self.selected != piece:
-                if self.selected:
-                    self.undraw_moves(self.selected.valid_moves)
+                if select := self.selected:
+                    self.undraw_moves(self.board_layout, self.selected.valid_moves)            
+                    self.selected.valid_moves.clear()
                 self.selected = piece
-
                 self.selected.valid_moves = self.draw_moves(self.selected.calc_moves(self.board_layout))
 
+                    
         #else:
 
 
 #move function: see if opponent piece's new pos puts enemy's king in danger 
+
 
