1fd36ee3e295cd42fbac98e049aae4d4000639c5
jjava1234 <81831375+jjava1234@users.noreply.github.com>
2021-09-18 00:53:09 -0400

    Update board.py

--- CHESS/chess/board.py
+++ CHESS/chess/board.py
@@ -1,150 +1,115 @@
-from itertools import permutations
-from constants import images
+from pieces import *
+from constants import *
 
-#LEFT / RIGHT
-def calc_LR(board, piece, movesList, x, y, dir):    
-    if -1 < x < 8 and -1 < y < 8:
-        if board[y][x] and board[y][x].color == piece.color:
-            return movesList        
-        movesList.append((x, y))
-        if not board[y][x]:
-            return calc_LR(board, piece, movesList, x+dir, y, dir)
-    return movesList
-
-# UP / DOWN
-def calc_UD(board, piece, movesList, x, y, dir): 
-    if -1 < x < 8 and -1 < y < 8:
-        if board[y][x] and board[y][x].color == piece.color:
-            return movesList
-        movesList.append((x, y))
-        if not board[y][x]:
-            return calc_UD(board, piece, movesList, x, y+dir, dir)
-    return movesList
+bPieces = [Rook, Horse, Bishop, Queen, King, Bishop, Horse, Rook]
+wPieces = bPieces[::-1]
 
 
-#DIAGONALS
-def calc_DI(board, piece, movesList, x, y, dir, reversed=1):
-    if -1 < x < 8 and -1 < y < 8:
-        if board[y][x] and board[y][x].color == piece.color:
-            return movesList
-        movesList.append((x,y))
-        if not board[y][x]:
-            return calc_DI(board, piece, movesList, x+dir, y+dir*reversed, dir, reversed)
-    return movesList
+class Board():
+    def __init__(self, window, game):
+        self.board_layout = []
+        self.selected = None
+        self.game = game
+        self.win = window
+        self.bK_pos = (4,0)
+        self.wK_pos = (3,7)
+        self.create_board()
+ 
+
+    def create_board(self):
+        self.win.fill((111,78,55))
+        for row in range(ROWS):
+            for col in range(row%2, COLS, 2):
+                pygame.draw.rect(self.win, (210,180,140), (col*80, row*80, 80, 80))
+        self.create_pieces()
+
+    def create_pieces(self):
+        board = self.board_layout
+        for row in range(8):
+            board.append([])
+            for col in range(8):
+                if row < 2:
+                    if row == 0:
+                        board[row].append(bPieces[col](col, row, (0,0,0), f"b{bPieces[col].__name__[0]}", self.win))
+                    else:
+                        board[row].append(0)
+
+                        #board[row].append(Pawn(col, row, (0,0,0), 1, "bP", self.win))
+                elif row > 5:
+                    if row == 7:
+                        board[row].append(wPieces[col](col, row, (255,255,255), f"w{wPieces[col].__name__[0]}", self.win))
+                    else: 
+                          board[row].append(0)
+
+                        #board[row].append(Pawn(col, row, (255,255,255), -1, "wP", self.win))
+                else:
+                    board[row].append(0)
+
+
+    def captureCheck(self, move):
+        x, y = move[0], move[1]
+        if piece := self.board_layout[y][x]:
+            piece = 0
     
+    def switchTurn(self, game):
+        game.turn = game.pColors[(game.pColors.index(game.turn)+1)%2]
 
-def calc_all_moves(self, board, LR = False, UD = False, DI = False):
-    moves = []
-    for dir in (-1, 1):
-        if LR and UD:
-            moves.extend(calc_UD(board, self, [], self.x, self.y+dir, dir))
-            moves.extend(calc_LR(board, self, [], self.x+dir, self.y, dir))
-        if DI:
-            moves.extend(calc_DI(board, self, [], self.x+dir, self.y+dir, dir))
-            moves.extend(calc_DI(board, self, [], self.x+dir, self.y+dir*-1, dir, -1))
-    
-    return moves
-
+    def movePiece(self, piece, oldIMAGES, move):
+        oldIMAGES.append((piece.x, piece.y))
         
-class Piece():
-    def __init__(self, x, y, color, piece_name, win):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.valid_moves = []
-        self.pName = piece_name
-        self.drawPIECE(win)
+        self.captureCheck(move)
 
-    def drawPIECE(self, win):
-        win.blit(images[self.pName], (((self.x*80 + 40) - images[self.pName].get_width()//2, (self.y*80 + 40) - images[self.pName].get_height()//2)))
+        if piece.pName[1] == "K":
+            pass
+        newX, newY = move[0], move[1]
+        self.board_layout[newY][newX], piece = piece, self.board_layout[newY][newX]
+        
+            #see if king's move gets him checked or 
 
+        piece = self.board_layout[newY][newX] 
+        self.undraw(self.board_layout, oldIMAGES)
+        piece.updatePIECE(self.win, (newX, newY))
+        
 
-    def updatePIECE(self, win, newPOS):
-        self.x, self.y = newPOS[0], newPOS[1]        
-        self.drawPIECE(win)
+        #self.kingCheck()
+        self.switchTurn(self.game)
+        
 
-class Pawn(Piece):
-    def __init__(self, x, y, color, dir, pName, win):
-        super().__init__(x, y, color, pName, win)
-        self.dir = dir
+    def drawMOVES(self, valid_moves):
+        for move in valid_moves:
+            pygame.draw.circle(self.win, (0,0,255), (move[0]*80+40, move[1]*80+40), 15)
+        return valid_moves
 
-    def ENEMEYPawnDJ(): #double jump
+    def undraw(self, board, positions, undrawMoves=False):
+        print(positions)
+        colors = [(210,180,140), (111,78,55)]
+        for pos in positions:
+            pygame.draw.rect(self.win, colors[(pos[0]+pos[1])%2], (pos[0]*80, pos[1]*80, 80, 80))     
+            
+            if undrawMoves and (piece := board[pos[1]][pos[0]]):
+                piece.drawPIECE(self.win)
+        self.selected.valid_moves.clear()
+
+    def captureCheck(self, piece):
         pass
-        #if self.ENEMYlastMove:
-            # or check it in move function
 
-    def calc_moves(self, board):
-        moves = []
-        #check for en passant moves 
-        if self.x not in (0,7):
-            for side in (-1,1): #left side/right side
-                sidePiece = board[self.y+self.dir][self.x+side]
-                if sidePiece and self.color != sidePiece.color and self.ENEMYpawnDJ(): 
-                        moves.append((self.x+side, self.y+self.dir))
-            
-        #check for captures moves
-        if self.y not in (0,7):
-            if (self.y == 1 and self.color == (0,0,0)) or (self.y == 6 and self.color == (255,255,255)):
-                if not board[self.y+self.dir*2][self.x]:
-                    moves.append((self.x, self.y+self.dir*2))
-            if not board[self.y+self.dir][self.x]:
-                moves.append((self.x, self.y+self.dir))
-        return moves
-
-
-class Rook(Piece):
-    def __init__(self, x, y, color, pName, win):
-        super().__init__(x, y, color, pName, win)
-    
-    def calc_moves(self, board):
-        print("hello?")
-        return calc_all_moves(self, board, True, True, False)
-
-#King and Knight starts with same letter
-class Horse(Piece):     
-    def __init__(self, x, y, color, pName, win):
-        super().__init__(x, y, color, pName, win)
-    
-    
-    def calc_moves(self, board, moves=[]):
-        dirs = [(-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
-        for dir in dirs:
-            if -1 < (y := self.y+dir[1]) < 8 and -1 < (x := self.x+dir[0]) < 8:
-                if not board[y][x] or self.color != board[y][x].color:     
-                    moves.append((x, y))
-        return moves
-
-class Bishop(Piece):
-    def __init__(self, x, y, color, pName, win):
-        super().__init__(x, y, color, pName, win)
-    
-    def calc_moves(self, board):
-        return calc_all_moves(self, board, False, False, True)
-
-
-class Queen(Piece):
-    def __init__(self, x, y, color, pName, win):
-        super().__init__(x, y, color, pName, win)
-
-    def calc_moves(self, board): 
-        return calc_all_moves(self, board, True, True, True)
-            
-
-class King(Piece):
-    def __init__(self, x, y, color, pName, win):
-        super().__init__(x, y, color, pName, win)
-
-    def calc_moves(self, board, moves=[]):
-        dirs = list(permutations([0, -1, -1, 1, 1], 2))
-        for dir in dirs:
-            x = self.x+dir[0]
-            y = self.y+dir[1]
-            if -1 < y < 8 and -1 < x < 8 :
+    def select(self, x, y):
+        print(x//80,y//80)
+        if (piece := self.board_layout[y//80][x//80]) and self.board_layout[y//80][x//80].color == self.game.turn:
+            if self.selected != piece:
+                if self.selected:
+                    self.undraw(self.board_layout, self.selected.valid_moves, True) #undraw moves           
+                self.selected = piece
+                self.selected.valid_moves = self.drawMOVES(self.selected.calc_moves(self.board_layout))
                 
-                if board[y][x] and self.color != board[y][x].color: 
-                    moves.append((self.x + dir[0], self.y+dir[1]))
-        print(moves)
-        return moves
+        elif validMOVES := self.selected.valid_moves:
+            if (x//80, y//80) in validMOVES:
+                self.movePiece(self.selected, self.selected.valid_moves, (x//80, y//80))
+
+                    
+        #else:
+
 
 #move function: see if opponent piece's new pos puts enemy's king in danger 
+
 
